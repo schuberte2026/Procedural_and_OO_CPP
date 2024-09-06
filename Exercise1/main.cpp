6ca83329f09eff23d14b36b03253db257372a622
Schubert <schuberte@msoe.edu>
2024-09-06 10:21:39 -0500

    Modified initialization statement.

--- Exercise1/main.cpp
+++ Exercise1/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main() {
     int number = 0;
+    double result = 0;
     cout << "Enter a number: " << endl;
     cin >> number;
     cout << endl;
 
-    double result = number * number * number;
+    result = number * number * number;
     cout << "The cube of " << number << " is " << result << endl;
 
     return 0;
