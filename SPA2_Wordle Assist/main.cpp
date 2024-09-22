05112eb4409048624605f4f536ea757fb72deeb0
Schubert <schuberte@msoe.edu>
2024-09-22 13:23:35 -0500

    Stubbed out helper methods to refer to when programming rest of lab.

--- SPA2_Wordle Assist/main.cpp
+++ SPA2_Wordle Assist/main.cpp
@@ -2,11 +2,16 @@
 #include <iostream>
 #include <cassert>
 
+const int MAX_SIZE = 3000;
 using std::string;
+using std::cout;
+using std::endl;
 
-bool can_match(string possible_answer, string guess, string letter_matches);
+bool can_match(const string& possible_answer, const string& guess, const string& letter_matches);
 
 void run_checks();
+
+bool iterate_letter_matches(const string& letter_matches);
 
 int main() {
     run_checks();
@@ -21,6 +26,40 @@ void run_checks() {
     assert( can_match("abcde", "uvwxy", "-----"));
 }
 
-bool can_match(string possible_answer, string guess, string letter_matches) {
+// (.) letter in guess matches letter in real word
+// (-) letter in guess not in real word
+// (?) letter appears in real world, but not at the guessed position
+bool can_match(const string& possible_answer, const string& guess, const string& letter_matches) {
+    iterate_letter_matches(letter_matches);
     return false;
+}
+
+bool iterate_letter_matches(const string& letter_matches) {
+    bool ret_bool = true;
+    for (int i = 0; i < letter_matches.size(); i++) {
+        if (letter_matches[i] == '.') {
+            //call helper method to check letter guess matches real world letter/position
+        }
+        else if (letter_matches[i] == '-') {
+            //call helper method to check that letter guess matches that it's not in word
+        }
+        else if (letter_matches[i] == '?') {
+            //call helper method to check that letter guess matches that letter appears in real world, but not in guessed position etc
+        }
+
+    }
+    return ret_bool;
+}
+
+
+void print_possible_guesses(std::string* possible_guesses, int pg_size, std::string* possible_answers, int pa_size) {
+    cout << "Possible guesses after : 0" << endl;
+    // cout << "Possible guesses after ";
+    // for (int i = 0; i < pg_size; i++) {
+    //     cout << possible_guesses[i] << ", ";
+    // }
+    // cout << ": " << pa_size << std::endl;
+    // for (int i = 0; i < pa_size; i++) {
+    //     cout << possible_answers[i] << endl;
+    // }
 }
