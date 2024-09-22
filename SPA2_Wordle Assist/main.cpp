b6a91de2c728a4a35f03e24ad782dcdefaa17f25
Schubert <schuberte@msoe.edu>
2024-09-22 13:29:38 -0500

    Further developed helper methods.

--- SPA2_Wordle Assist/main.cpp
+++ SPA2_Wordle Assist/main.cpp
@@ -9,9 +9,15 @@ using std::endl;
 
 bool can_match(const string& possible_answer, const string& guess, const string& letter_matches);
 
+bool check_exact_match(const string& possible_answer, const string& guess, int index_to_check);
+
+bool check_no_occurence(const string& possible_answer, const string& guess, int index_to_check);
+
+bool check_misplaced_match(const string& possible_answer, const string& guess, int index_to_check);
+
 void run_checks();
 
-bool iterate_letter_matches(const string& letter_matches);
+bool iterate_letter_matches(const string& possible_answer, const string& guess, const string& letter_matches);
 
 int main() {
     run_checks();
@@ -30,21 +36,24 @@ void run_checks() {
 // (-) letter in guess not in real word
 // (?) letter appears in real world, but not at the guessed position
 bool can_match(const string& possible_answer, const string& guess, const string& letter_matches) {
-    iterate_letter_matches(letter_matches);
+    iterate_letter_matches(possible_answer, guess, letter_matches);
     return false;
 }
 
-bool iterate_letter_matches(const string& letter_matches) {
+bool iterate_letter_matches(const string& possible_answer, const string& guess, const string& letter_matches) {
     bool ret_bool = true;
     for (int i = 0; i < letter_matches.size(); i++) {
         if (letter_matches[i] == '.') {
             //call helper method to check letter guess matches real world letter/position
+            check_no_occurence(possible_answer, guess, i);
         }
         else if (letter_matches[i] == '-') {
             //call helper method to check that letter guess matches that it's not in word
+            check_no_occurence(possible_answer, guess, i);
         }
         else if (letter_matches[i] == '?') {
             //call helper method to check that letter guess matches that letter appears in real world, but not in guessed position etc
+            check_misplaced_match(possible_answer, guess, i);
         }
 
     }
