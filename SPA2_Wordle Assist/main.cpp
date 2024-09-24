67b64a51d31d8ef32c7621c906df72742ece1003
Schubert <schuberte@msoe.edu>
2024-09-24 14:45:09 -0500

    Reduced method to meet 15 line requirement.

--- SPA2_Wordle Assist/main.cpp
+++ SPA2_Wordle Assist/main.cpp
@@ -22,6 +22,8 @@ void read_data();
 
 bool iterate_letter_matches(const string& possible_answer, const string& guess, const string& letter_matches);
 
+bool check_letter_matches(const string& possible_answer, const string& guess, const string& letter_matches, size_t i);
+
 //void print_possible_guesses(std::string* possible_guesses, int pg_size, std::string* possible_answers, int pa_size);
 void print_possible_guesses();
 
@@ -47,6 +49,7 @@ void read_data() {
     string possible_guesses[MAX_SIZE];
     string word;
     int num_guesses = 0;
+    int guess_count = 0;
 
     //reads in words from file until "END" occurs, then it breaks out of while loop
     int i = 0;
@@ -59,6 +62,7 @@ void read_data() {
     int j = 0;
     while (cin >> word ) {
         guesses[j] = word; //takes in next line as a "guess"
+        guess_count++;
         cin >> word; // assumes that there will be letter match as next line
         letter_matches[j] = word;
         j++;
@@ -103,7 +107,7 @@ void read_data() {
             cout << guess;
             cur_index++;
 
-            if (cur_index < num_guesses) {
+            if (cur_index < guess_count) {
                 cout << ", ";
             }
         }
@@ -131,29 +135,29 @@ bool can_match(const string& possible_answer, const string& guess, const string&
 
 bool iterate_letter_matches(const string& possible_answer, const string& guess, const string& letter_matches) {
     for (size_t i = 0; i < letter_matches.size(); i++) {
-        if (letter_matches[i] == '.') {
-            //checks if guess character matches character in same position in possible_answer
-            if(!check_exact_match(possible_answer, guess, i)) {
-                return false;
-            }
-        }
-        else if (letter_matches[i] == '-') {
-            //checks if guess character is not contained in possible_answer
-            if(!check_no_match(possible_answer, guess[i])) {
-                return false;
-            }
-        }
-        else if (letter_matches[i] == '?') {
-            //checks if guess character appears in possible_answer, but the position guessed wasn't correct
-            if(!check_misplaced_match(possible_answer, guess, i)) {
-                return false;
-            }
+        if(!check_letter_matches(possible_answer, guess, letter_matches, i)) {
+            return false;
         }
     }
     return true;
 }
 
+bool check_letter_matches(const string& possible_answer, const string& guess, const string& letter_matches, size_t i) {
+    switch (letter_matches[i]) {
+        case '.':
+            return check_exact_match(possible_answer, guess, i);
+        case '-':
+            return check_no_match(possible_answer, guess[i]);
+        case '?':
+            return check_misplaced_match(possible_answer, guess, i);
+        default:
+            return false;
+    }
+}
+
+
 bool check_exact_match(const string& possible_answer, const string& guess, int index_to_check) {
+    //checks if guess character matches character in same position in possible_answer
     const char pa_char = possible_answer[index_to_check];
     const char g_char = guess[index_to_check];
 
@@ -164,11 +168,13 @@ bool check_exact_match(const string& possible_answer, const string& guess, int i
 }
 
 bool check_no_match(const string& possible_answer, const char& c) {
+    //checks if guess character is not contained in possible_answer
     //returns whether the character from the guess word is in possible_answer
     return possible_answer.find(c) == string::npos;
 }
 
 bool check_misplaced_match(const string& possible_answer, const string& guess, size_t index_to_check) {
+    //checks if guess character appears in possible_answer, but the position guessed wasn't correct
     char g_char = guess[index_to_check];
 
     for (size_t i = 0; i < possible_answer.size(); i++) {
@@ -179,19 +185,7 @@ bool check_misplaced_match(const string& possible_answer, const string& guess, s
     }
     return false;
 }
-/*
-void print_possible_guesses(std::string* possible_guesses, int pg_size, std::string* possible_answers, int pa_size) {
-    cout << "Possible guesses after : 0" << endl;
-    // cout << "Possible guesses after ";
-    // for (int i = 0; i < pg_size; i++) {
-    //     cout << possible_guesses[i] << ", ";
-    // }
-    // cout << ": " << pa_size << std::endl;
-    // for (int i = 0; i < pa_size; i++) {
-    //     cout << possible_answers[i] << endl;
-    // }
-}
-*/
+
 
 
 void print_possible_guesses() {
